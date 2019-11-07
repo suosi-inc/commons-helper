0e93e8c9ce18979033d86aaf1eb2323f67a80016
niuchaoqun <smallmenu@gmail.com>
2019-11-07 17:58:09 +0800

    * update

--- src/main/java/com/github/suosi/commons/helper/Static.java
+++ src/main/java/com/github/suosi/commons/helper/Static.java
@@ -16,7 +16,9 @@ import java.time.format.DateTimeFormatter;
  */
 public class Static {
 
-    private static String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+
+    public static final String DEFAULT_DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
     /**
      * 返回当前 Unix 时间戳（毫秒）
